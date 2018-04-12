52a77857f694c84cb73b0c7784191075616fd884
SakuraOno <skr.o_jun11@icloud.com>
2018-04-12 14:07:23 +0900

    Music

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+   
+   var audioPlayer : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   @IBAction func natsuPlayButton(){
+     setupAudioPlayer(soundName: "natsu", type: "mp3")
+      audioPlayer.play( )
+      
+   }
+   
+   @IBAction func amePlayButton(){
+      setupAudioPlayer(soundName: "ame", type: "mp3")
+      audioPlayer.play()
+      
+   }
+   @IBAction func akiPlayButton(){
+    setupAudioPlayer(soundName: "aki", type: "mp3")
+      audioPlayer.play()
+      
+   }
+   func setupAudioPlayer (soundName : String, type : String) {
+      
+      let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)!
+      let fileURL = URL(fileURLWithPath: soundFilePath)
+      
+      do  {
+         
+         audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+      } catch{
+         
+         print("音楽ファイルが読み込めませんでした")
+         
+         
+      }
+      
 }
 
+}
